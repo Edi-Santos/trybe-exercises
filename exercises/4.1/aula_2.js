0e36148ddfffbd91ed9ee803e17b6b593bb2cd15
Ed Santos <edimarsantos075@gmail.com>
2021-04-22 18:56:58 -0300

    Completing exercises of the second lesson

---
+++ exercises/4.1/aula_2.js
@@ -0,0 +1,25 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log("variável tipo " + typeof patientId);
+console.log("variável tipo " + typeof isEnrolled);
+console.log("variável tipo " + typeof patientInfo);
+console.log("variável tipo " + typeof patientEmail);
+
+console.log("variável tipo " + typeof patientAge);
+
+patientId = '50';
+console.log("variável tipo " + typeof patientId);
+
+let base = 5;
+let altura = 8;
+let area = base * altura;
+console.log("área = " + area);
+
+let perimetro = ((base * 2)+(altura * 2));
+console.log("perímetro = " + perimetro);
