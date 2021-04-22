5f7e287b13ccfba7596c6e803d28eeac18aab924
Ed Santos <edimarsantos075@gmail.com>
2021-04-22 15:09:50 -0300

    Starting the fourth block and completing the exercise of the first lesson

---
+++ exercises/4.1/aula_1.js
@@ -0,0 +1,13 @@
+const name = "Edimar";
+const birthCity = "Rio de Janeiro";
+var birthYear = 1997;
+
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+// Erro forçado 
+birthCity = "Barcelona";
+console.log(birthCity);
