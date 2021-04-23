8faaeaff3070475e8b5a78d8d1d273266031e035
Ed Santos <edimarsantos075@gmail.com>
2021-04-23 12:17:45 -0300

    Completing exercise 1

---
+++ exercises/4.1/exercicios_b-4.js
@@ -0,0 +1,14 @@
+let a = 5;
+let b = 8;
+
+let soma = a + b;
+let subtr = a - b;
+let mult = a * b;
+let divide = a / b;
+let mod = a % b;
+
+console.log("adição = " + soma);
+console.log("subtração = " + subtr);
+console.log("multiplicação = " + mult);
+console.log("divisão = " + divide);
+console.log("modulação = " + mod);
