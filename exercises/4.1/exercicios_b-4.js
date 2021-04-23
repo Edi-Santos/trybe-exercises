bdd5cd7a60c851f3899b4be4d9b9cef32681bb25
Ed Santos <edimarsantos075@gmail.com>
2021-04-23 12:29:25 -0300

    Completing exercise 2

--- exercises/4.1/exercicios_b-4.js
+++ exercises/4.1/exercicios_b-4.js
@@ -1,3 +1,4 @@
+// Exercício 1
 let a = 5;
 let b = 8;
 
@@ -12,3 +13,10 @@ console.log("subtração = " + subtr);
 console.log("multiplicação = " + mult);
 console.log("divisão = " + divide);
 console.log("modulação = " + mod);
+
+// Exercício 2
+if(a > b) {
+    console.log("\n a é maior que b");
+}else {
+    console.log("\n b é maior que a");
+}
