cd65a4d5d265d20b4501ac481db21f44dec5cff2
Ed Santos <edimarsantos075@gmail.com>
2021-04-23 21:31:15 -0300

    Completing first exercise of the block 4.2

---
+++ exercises/4.2/exercicios.js
@@ -0,0 +1,7 @@
+// Exercícios do bloco 4.2
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Exercícios 1
+for(index = 0; index < numbers.length; index += 1) {
+    console.log(numbers[index]);
+}
