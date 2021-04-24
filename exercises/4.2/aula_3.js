960b4358b70a61b8a2a3b3b9719fa066135daa30
Ed Santos <edimarsantos075@gmail.com>
2021-04-23 21:18:49 -0300

    Showing the second possibility with for/of

--- exercises/4.2/aula_3.js
+++ exercises/4.2/aula_3.js
@@ -4,3 +4,11 @@ let numeros = [1, 2, 3, 4, 5];
 for(let numero of numeros) {
     console.log(numero);
 }
+
+console.log('\n');
+
+// Laço for/of em string
+let word = "Hello";
+for(let letter of word) {
+    console.log(letter);
+}
