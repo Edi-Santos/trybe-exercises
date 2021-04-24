abbc83b2687fbb5e0972aae15bc3349a7654533e
Ed Santos <edimarsantos075@gmail.com>
2021-04-23 22:19:54 -0300

    Completing fourth exercise of the block 4.2

--- exercises/4.2/exercicios.js
+++ exercises/4.2/exercicios.js
@@ -26,3 +26,13 @@ for(index = 0; index < numbers.length; index += 1) {
 }
 let media = somatorio / numbers.length;
 console.log(`média = ${media}`);
+
+console.log('\n');
+
+// Exercício 4 - Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20"
+console.log('Exercício 4:');
+if(media > 20) {
+    console.log('valor maior que 20');
+}else {
+    console.log('valor menor ou igual a 20');
+}
