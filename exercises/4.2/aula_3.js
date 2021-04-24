0d1e98d8c21aa13d70b6761cb785d62ff14ed8d9
Ed Santos <edimarsantos075@gmail.com>
2021-04-23 21:13:02 -0300

    Showing the first possibility with for/of

---
+++ exercises/4.2/aula_3.js
@@ -0,0 +1,6 @@
+// Laço for/of permite percorrer objetos iteráveis como arrays e Strings
+// Laço for/of em array
+let numeros = [1, 2, 3, 4, 5];
+for(let numero of numeros) {
+    console.log(numero);
+}
