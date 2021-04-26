80498a70e6ed9f98cef7cf276704d45af5e97a8b
Ed Santos <edimarsantos075@gmail.com>
2021-04-26 20:42:54 -0300

    Completing second exercise of the block 4.3

--- exercises/4.3/exercicios_b-4.3.js
+++ exercises/4.3/exercicios_b-4.3.js
@@ -9,12 +9,31 @@ n = 5
 *****
 */
 let n = 5;
-let a = '*';
+let ast = '*';
 let linha = '';
 
 for(let i = 0; i < n; i += 1){
-    linha = linha + a;
+    linha = linha + ast;
     for(let ii = 0; ii < n; ii +=1) {
         console.log(linha);
     };
+};
+
+// Exercício 2 - Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+/*
+n = 5
+
+*
+**
+***
+****
+*****
+*/
+let n2 = 5;
+let ast2 = '*';
+let linha2 = '';
+
+for(let i = 0; i < n2; i += 1) {
+    linha2 += ast2;
+    console.log(linha2);
 };
