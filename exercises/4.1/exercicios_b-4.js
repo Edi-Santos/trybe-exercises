d53660dca9eb940b3191ef4e4ea49f7e5c6f154b
Ed Santos <edimarsantos075@gmail.com>
2021-04-26 12:35:33 -0300

    Completing exercise 7

--- exercises/4.1/exercicios_b-4.js
+++ exercises/4.1/exercicios_b-4.js
@@ -1,4 +1,5 @@
 // Exercício 1 - Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+console.log('Exercício 1:');
 let a = 5;
 let b = 8;
 
@@ -14,40 +15,51 @@ console.log("multiplicação = " + mult);
 console.log("divisão = " + divide);
 console.log("modulação = " + mod);
 
+console.log('\n');
+
 // Exercício 2 - Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+console.log('Exercício 2:');
 if(a > b) {
-    console.log("\n a é maior que b");
+    console.log("a é maior que b");
 }else {
-    console.log("\n b é maior que a");
+    console.log("b é maior que a");
 }
 
+console.log('\n');
+
 // Exercício 3 - Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.
+console.log('Exercício 3:');
 let a3 = 15;
 let b3 = 3;
 let c3 = 16;
 
 if(a3 > b3 && a > c3) {
-    console.log("\n a3 é o maior número \n");
+    console.log("a3 é o maior número");
 }else if(b3 > a3 && b3 > c3) {
-    console.log("\n b3 é o maior número \n");
+    console.log("b3 é o maior número");
 }else if(c3 > a3 && c3 > b3) {
-    console.log("\n c3 é o maior número \n");
+    console.log("c3 é o maior número");
 }else {
-    console.log('\n Error!');
+    console.log('Error!');
 }
 
+console.log('\n');
+
 // Exercício 4 - Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+console.log('Exercício 4:');
 let a4 = -3;
 
 if(a4 > 0) {
-    console.log("positive \n");
+    console.log("positive");
 }else if(a4 < 0) {
-    console.log("negative \n");
+    console.log("negative");
 }else {
-    console.log("zero \n");
+    console.log("zero");
 }
 
+console.log('\n');
 // Exercício 5 - Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+console.log('Exercício 5:');
 let angulo1 = 60;
 let angulo2 = -61;
 let angulo3 = 60;
@@ -75,6 +87,7 @@ switch(triangulo) {
 console.log('\n');
 
 // Exercício 6 - Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+console.log('Exercício 6:');
 let pecaXadrez = 'Rei';
 
 switch(pecaXadrez.toLowerCase()) {
@@ -98,4 +111,26 @@ switch(pecaXadrez.toLowerCase()) {
         break;
     default:
         console.log('Valor inválido.');
+}
+
+console.log('\n');
+
+// Exercício 7 - Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+console.log('Exercício 7:');
+let porcentagem = 70;
+
+if(porcentagem >= 90 && porcentagem <= 100) {
+    console.log('nota - A');
+}else if(porcentagem < 90 && porcentagem >= 80) {
+    console.log('nota - B');
+}else if(porcentagem < 80 && porcentagem >= 70) {
+    console.log('nota - C');
+}else if(porcentagem < 70 && porcentagem >= 60) {
+    console.log('nota - D');
+}else if(porcentagem < 60 && porcentagem >= 50) {
+    console.log('nota - E');
+}else if(porcentagem < 50 && porcentagem >= 0) {
+    console.log('nota - F');
+}else {
+    console.log('error');
 }
