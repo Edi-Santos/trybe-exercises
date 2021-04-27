b8c3211079698d050cd19f44aa95516069333454
Ed Santos <edimarsantos075@gmail.com>
2021-04-27 16:15:16 -0300

    Completing third exercise

--- exercises/4.4/aula_1.js
+++ exercises/4.4/aula_1.js
@@ -16,3 +16,6 @@ let player = {
 
 // Acesse a chave name, lastName e age. Concatene as informações para fazer um console.log no seguinte formato: "A jogadora Marta Silva tem 34 anos de idade".
 console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.');
+
+//Adicione ao objeto a chave bestInTheWorld e atribua a esta chave um array contendo as datas em que a jogadora Marta foi considerada a melhor do mundo.
+player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
