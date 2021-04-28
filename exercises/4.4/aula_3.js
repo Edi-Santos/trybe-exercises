d9972fdf24c30c7497e7c5c4e5691267d7953848
Ed Santos <edimarsantos075@gmail.com>
2021-04-28 11:08:41 -0300

    Completing the first and second parts of the exercise

---
+++ exercises/4.4/aula_3.js
@@ -0,0 +1,27 @@
+// Pegue cada um dos exercícios da primeira parte das nossas aulas de JavaScript e faça com que todos eles sejam funções de um mesmo arquivo. As variáveis que você define no começo de cada arquivo devem ser removidas e transformadas em parâmetros das funções. Por exemplo:
+
+// Adição
+// let a = 5;
+// let b = 2;
+
+// a + b;
+
+// ... se transforma em:
+
+// function sum(a, b) {
+//     return a + b;
+// }
+console.log('soma:');
+function soma(a, b) {
+    console.log(a + b);
+};
+soma(2, 8);
+console.log('\n');
+
+console.log('subtração:');
+function subtr(a, b) {
+    console.log(a - b);
+};
+subtr(6, 7);
+console.log('\n');
+
