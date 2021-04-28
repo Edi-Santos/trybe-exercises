d9053bb9247c09e784762519cff485f373e1998f
Ed Santos <edimarsantos075@gmail.com>
2021-04-28 11:23:15 -0300

    Completing third part of the exercise

--- exercises/4.4/aula_3.js
+++ exercises/4.4/aula_3.js
@@ -11,6 +11,8 @@
 // function sum(a, b) {
 //     return a + b;
 // }
+
+// Adição
 console.log('soma:');
 function soma(a, b) {
     console.log(a + b);
@@ -18,10 +20,22 @@ function soma(a, b) {
 soma(2, 8);
 console.log('\n');
 
+
+// Subtração
 console.log('subtração:');
 function subtr(a, b) {
     console.log(a - b);
 };
 subtr(6, 7);
 console.log('\n');
+
+
+// Multiplicação
+console.log('multiplicação:');
+function mult(a, b) {
+    console.log(a * b);
+};
+mult(3, 8);
+console.log('\n');
+
 
