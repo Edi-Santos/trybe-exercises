bba164f82445b2244bb2eb58e9633e281f5543a0
Ed Santos <edimarsantos075@gmail.com>
2021-04-27 22:27:42 -0300

    Completing first exercise

---
+++ exercises/4.4/aula_2.js
@@ -0,0 +1,16 @@
+// Usando o objeto abaixo, utilize For/in e imprima um console log 'Olá xxxxx' para cada nome no objeto.
+
+//  let names = {
+//    person1: 'João',
+//    person2: 'Maria',
+//    person3: 'Jorge'
+//  }
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge'
+};
+
+for (let key in names) {
+    console.log('Olá ' + names[key]);
+};
