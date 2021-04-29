cc703cfb170598d566d9647b8599ae535144d6b3
Ed Santos <edimarsantos075@gmail.com>
2021-04-29 18:46:35 -0300

    Completing the first and second exercises of the first lesson

---
+++ exercises/5.1/js/aula_1.js
@@ -0,0 +1,10 @@
+var paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+// Exercício 1 - Recupere o elemento que contém o título da página e faça algo com ele, como alterá-lo para o nome do seu filme favorito.
+let pageTitle = document.getElementById('page-title');
+pageTitle.innerText = 'Mortal Kombat';
+
+// Exercício 2 - Agora recupere o segundo parágrafo e use sua criatividade para alterá-lo.
+let secondParagraph = document.getElementById('second-paragraph');
+secondParagraph.style.border = 'solid';
