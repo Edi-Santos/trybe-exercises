65bd90fe9c34ff6892c0d8f621da842c22593fca
Ed Santos <edimarsantos075@gmail.com>
2021-04-29 19:32:19 -0300

    Completing third exercise of the first lesson and adding a boxShadow

--- exercises/5.1/js/aula_1.js
+++ exercises/5.1/js/aula_1.js
@@ -7,4 +7,9 @@ pageTitle.innerText = 'Mortal Kombat';
 
 // Exercício 2 - Agora recupere o segundo parágrafo e use sua criatividade para alterá-lo.
 let secondParagraph = document.getElementById('second-paragraph');
-secondParagraph.style.border = 'solid';
+secondParagraph.style.border = '1px solid';
+secondParagraph.style.boxShadow = '5px 5px 5px gray';
+
+// Exercício 3 - Por fim, recupere o subtítulo e altere-o também.
+let subtitle = document.getElementById('subtitle');
+subtitle.style.fontFamily = 'sans-serif';
