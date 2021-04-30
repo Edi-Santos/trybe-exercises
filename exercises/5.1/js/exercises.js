7b898ccd774c41c8856e1c6f95f959f2977f5dcc
Ed Santos <edimarsantos075@gmail.com>
2021-04-30 13:57:06 -0300

    Styling the right section

--- exercises/5.1/js/exercises.js
+++ exercises/5.1/js/exercises.js
@@ -18,3 +18,17 @@ let sectionETH3 = document.querySelectorAll('.emergency-tasks h3');
 for (let i = 0; i < sectionETH3.length; i++) {
     sectionETH3[i].style.background = 'rgb(165, 83, 147)';
 }
+
+// Section .no-emergency-tasks
+let sectionNTE = document.getElementsByClassName('no-emergency-tasks');
+sectionNTE[0].style.background = 'yellow';
+
+let sectionNTEDiv = document.querySelectorAll('.no-emergency-tasks div');
+for (let i = 0; i < sectionNTEDiv.length; i++) {
+    sectionNTEDiv[i].style.background = 'yellow';
+}
+
+let sectionNTEH3 = document.querySelectorAll('.no-emergency-tasks h3');
+for (let i = 0; i < sectionNTEH3.length; i++) {
+    sectionNTEH3[i].style.background = 'black';
+}
