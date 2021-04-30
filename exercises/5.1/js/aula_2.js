c0a8959666cd3759da10e46fa4ee810fdb8748a9
Ed Santos <edimarsantos075@gmail.com>
2021-04-30 10:59:50 -0300

    Exchanging some IDs for Classes and Completing first exercise of the second and third lessons

---
+++ exercises/5.1/js/aula_2.js
@@ -0,0 +1,6 @@
+// Adicione uma classe igual para os dois parágrafos.
+
+// Exercício 1 - Recupere os seus parágrafos via código JavaScript, usando a função getElementsByClassName;
+let prg = document.getElementsByClassName("paragraph");
+console.log(prg)
+
