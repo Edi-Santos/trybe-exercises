7ffacce849626c0b0433194dd86dfebf25f77c01
Ed Santos <edimarsantos075@gmail.com>
2021-04-30 13:02:20 -0300

    Stiling the header using JavaScript

---
+++ exercises/5.1/js/exercises.js
@@ -0,0 +1,6 @@
+// Header
+let header = document.querySelector('header');
+header.style.backgroundColor = 'rgb(79, 199, 95)';
+
+let headerH1 = document.querySelector('header h1');
+headerH1.style.background = 'rgb(79, 199, 95)'
