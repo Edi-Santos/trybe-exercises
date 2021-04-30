471e014a7cb046b49b8f903ffb2530013882cb6c
Ed Santos <edimarsantos075@gmail.com>
2021-04-30 16:21:46 -0300

    Fixing the indentation of HTML and completing second exercise

--- exercises/5.2/js/aula_1.js
+++ exercises/5.2/js/aula_1.js
@@ -1,3 +1,7 @@
 // Exercise 1 - Acesse o elemento elementoOndeVoceEsta.
 let acces = document.getElementById('elementoOndeVoceEsta');
 console.log(acces);
+
+// Exercise 2 - Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+let acces2 = document.getElementById('elementoOndeVoceEsta').parentNode;
+acces2.style.background = 'Cyan';
