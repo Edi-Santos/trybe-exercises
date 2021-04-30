ff713eed4a04effc9350fd000395cb29a922a033
Ed Santos <edimarsantos075@gmail.com>
2021-04-30 19:53:54 -0300

    Completing exercise 3 of the first lesson

--- exercises/5.2/js/aula_1.js
+++ exercises/5.2/js/aula_1.js
@@ -5,3 +5,8 @@ console.log(acces);
 // Exercise 2 - Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
 let acces2 = document.getElementById('elementoOndeVoceEsta').parentNode;
 acces2.style.background = 'Cyan';
+
+// Exercise 3 - Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+let firstSonOfSon = document.getElementById('primeiroFilhoDoFilho');
+firstSonOfSon.innerHTML = 'Div primeiroFilhoDoFilho';
+
