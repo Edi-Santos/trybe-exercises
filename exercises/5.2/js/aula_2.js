129b16924b39240328bceaf84c81041a9b8cc552
Ed Santos <edsantos2822@gmail.com>
2021-05-01 14:09:46 -0300

    Completing exercise 2

--- exercises/5.2/js/aula_2.js
+++ exercises/5.2/js/aula_2.js
@@ -3,6 +3,15 @@ let father = document.querySelector("#pai");
 
 let creatingElement = document.createElement('div');
 creatingElement.innerHTML = '<p>Criando irmão de elementoOndeVoceEsta</p>';
-creatingElement.id = "adiciona-irmao";
+creatingElement.className = "adiciona-irmao";
 
 father.appendChild(creatingElement);
+
+// Exercício 2 - Crie um filho para elementoOndeVoceEsta.
+let elementoOndeVoceEsta = document.getElementById("elementoOndeVoceEsta");
+
+let creatingElement2 = document.createElement('div');
+creatingElement2.innerHTML = '<p>Criando filho de elementoOndeVoceEsta</p>';
+creatingElement2.className = 'adiciona-filho';
+
+elementoOndeVoceEsta.appendChild(creatingElement2);
