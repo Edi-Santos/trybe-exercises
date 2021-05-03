fd6fc393a953b59161d980537d8910a93b5b47c4
Ed Santos <edsantos2822@gmail.com>
2021-05-03 12:14:43 -0300

    Completing third and fourth part of the exercise

--- exercises/5.2/js/aula_3.js
+++ exercises/5.2/js/aula_3.js
@@ -5,3 +5,9 @@ removeFirst.remove();
 
 let removeSonOfSon = document.getElementById('elementoOndeVoceEsta').lastElementChild;
 removeSonOfSon.remove();
+
+let removeThird = document.querySelector('#pai').firstElementChild.nextElementSibling;
+removeThird.remove();
+
+let removeFourth = document.getElementById('pai').lastElementChild;
+removeFourth.remove();
