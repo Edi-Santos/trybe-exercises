35fa5097b3098f4edcb83ea4dbe97b9ddba2d230
Ed Santos <edsantos2822@gmail.com>
2021-05-27 20:15:28 -0300

    Completing exercise 3

--- exercises/7.1/exercicio3.js
+++ exercises/7.1/exercicio3.js
@@ -4,3 +4,11 @@
     · Uma variável clickCount no arquivo JavaScript que acumule o número de clicks no botão;
     · Um campo no HTML que vá atualizando a quantidade de clicks no botão conforme a variável clickCount é atualizada.
 */
+
+const pegaSpan = document.querySelector('span');
+const pegaBtn = document.querySelector('button');
+
+let vez = 0;
+pegaBtn.addEventListener('click', () => {
+  pegaSpan.innerHTML = vez += 1;
+});
