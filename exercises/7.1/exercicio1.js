0ec7529254d05c1b62c442e21d6745c48e5bca9f
Ed Santos <edsantos2822@gmail.com>
2021-05-26 22:08:33 -0300

    Completing exercise 1

--- exercises/7.1/exercicio1.js
+++ exercises/7.1/exercicio1.js
@@ -1,20 +1,20 @@
 /* 
-1 - Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
-	· Modifique a estrutura da função para que ela seja uma arrow function .
-	· Modifique as concatenações para template literals .
-	· Copie o código abaixo.
+	Exercício 1 - Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
+		· Modifique a estrutura da função para que ela seja uma arrow function .
+		· Modifique as concatenações para template literals .
+		· Copie o código abaixo.
 */
 
-function testingScope(escopo) {
+const testingScope = (escopo) => {
 	if (escopo === true) {
-		var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-		ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+		let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
+		ifScope = ifScope + ` ótimo, fui utilizada no escopo !`;
 		console.log(ifScope);
 	} else {
-		var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+		let elseScope = `Não devo ser utilizada fora meu escopo (else)`;
 		console.log(elseScope);
 	}
-	console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+	// console.log(ifScope + ` o que estou fazendo aqui ? :O`); // Se necessário esta linha pode ser removida.
 }
 
 testingScope(true);
