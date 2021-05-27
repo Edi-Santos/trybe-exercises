ab2c9070ee33c1435d2800588b8eece85868ef46
Ed Santos <edsantos2822@gmail.com>
2021-05-27 12:23:58 -0300

    Completing exercise 2

--- exercises/7.1/exercicio2.js
+++ exercises/7.1/exercicio2.js
@@ -1,8 +1,8 @@
 /* 
 Abaixo, você verá algumas especificações de algoritmos para desenvolver. É fundamental que você utilize o que aprendeu sobre let, const, arrow functions, template literals e ternary operator.
-		Exercício 1 - Crie uma função que receba um número e retorne seu fatorial.
-				· Na matemática, o fatorial de um número não negativo N, com a notação N!, é o produto de todos os inteiros menores ou iguais a N. Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
-				· Bônus (opcional): tente fazer o mesmo exercício de forma recursiva. Spoiler: É possível resolver com uma linha usando ternary operator.
+	Exercício 1 - Crie uma função que receba um número e retorne seu fatorial.
+		· Na matemática, o fatorial de um número não negativo N, com a notação N!, é o produto de todos os inteiros menores ou iguais a N. Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
+		· Bônus (opcional): tente fazer o mesmo exercício de forma recursiva. Spoiler: É possível resolver com uma linha usando ternary operator.
 */
 
 const fatorial = (numero) => {
@@ -18,3 +18,24 @@ const fatorial = (numero) => {
 }
 
 console.log(fatorial(10));
+
+/* 
+	Exercício 2 - Crie uma função que receba uma frase e retorne qual a maior palavra.
+		· Exemplo:
+			» longestWord("Antônio foi no banheiro e não sabemos o que aconteceu") // retorna 'aconteceu'
+ */
+
+const longestWord = _ => {
+	let separando = _.split(' ');
+	let maiorPalavra = '';
+
+	for (let index = 0; index < separando.length; index += 1) {
+		if (maiorPalavra.length < separando[index].length) {
+			maiorPalavra = separando[index];
+		}
+	}
+
+	return maiorPalavra;
+};
+
+console.log(`A maior palavra da frase é: ${longestWord('Antônio foi no banheiro e não sabemos o que aconteceu')}`);
