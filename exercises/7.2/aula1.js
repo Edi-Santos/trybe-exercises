3d1f9ff8d351f2fc1bbcdffddd2222e2d81f4180
Ed Santos <edsantos2822@gmail.com>
2021-05-28 15:23:43 -0300

    Adding content within aula1.js

---
+++ exercises/7.2/aula1.js
@@ -0,0 +1,26 @@
+// Adicionando novas chaves e valores num objeto.
+
+const customer1 = {
+  firstName: 'Roberto',
+  age: 22,
+  job: 'Teacher',
+};
+
+console.log('Primeiro console:');
+console.log(customer1);
+
+customer1.lastName = 'Faria';
+console.log('Segundo console:');
+console.log(customer1);
+
+const customer2 = {
+  firstName: 'Maria',
+  age: 23,
+  job: 'Medic',
+};
+
+console.log('Terceiro console:');
+console.log(customer2);
+customer2['lastName'] = 'Silva';
+console.log('Quarto console:');
+console.log(customer2);
