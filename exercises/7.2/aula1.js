3c2547d4d0f970e5bfb773269c834badf43f2be0
Ed Santos <edsantos2822@gmail.com>
2021-05-28 15:25:36 -0300

    Adding an exercise to aula1.js

--- exercises/7.2/aula1.js
+++ exercises/7.2/aula1.js
@@ -24,3 +24,5 @@ console.log(customer2);
 customer2['lastName'] = 'Silva';
 console.log('Quarto console:');
 console.log(customer2);
+
+// Agora, para praticar, crie uma função que receba três parâmetros, sendo eles: um objeto, o nome de uma chave e o seu valor. O retorno dessa função deve ser o objeto já com a nova chave adicionada nele.
