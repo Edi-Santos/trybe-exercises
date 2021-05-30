97dfac3c53b4c76b940bc2b5814e77b35d376a6f
Ed Santos <edsantos2822@gmail.com>
2021-05-30 20:28:12 -0300

    Completing exercise

--- exercises/7.2/aula2.js
+++ exercises/7.2/aula2.js
@@ -15,4 +15,39 @@ const student2 = {
   Git: 'Bom', // chave adicionada
 };
 
-// Tente criar uma função que exiba as habilidades do objeto student . Cada habilidade deve ser exibida no formato "Nome da habilidade, Nível: valor da chave referente à habilidade". Após tentar criar a função, veja a sugestão de resposta logo abaixo e compare com a sua solução.
+// Tente criar uma função que exiba as habilidades do objeto student. Cada habilidade deve ser exibida no formato "Nome da habilidade, Nível: valor da chave referente à habilidade". Após tentar criar a função, veja a sugestão de resposta logo abaixo e compare com a sua solução.
+
+const showSkills = (obj) => {
+  let storingInfo = ``;
+  storingInfo = Object.keys(obj);
+  for (let i = 0; i < storingInfo.length; i += 1) {
+    console.log(`${storingInfo[i]}, Nível ${obj[storingInfo[i]]}`)
+  };
+};
+
+console.log(showSkills(student1));
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const listSkills = (student) => {
+  const arrayOfSkills = Object.keys(student);
+  for(index in arrayOfSkills){
+    console.log(`${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);
+  }
+};
+
+console.log('Estudante 1');
+listSkills(student1);
+
+console.log('Estudante 2');
+listSkills(student2); */
