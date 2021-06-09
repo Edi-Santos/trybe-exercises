b32817b58ff5cada3b844c170f4eae707e2f2a7b
Ed Santos <edsantos2822@gmail.com>
2021-06-08 21:42:52 -0300

    Adding content

--- exercises/9.2/aula1-promises.js
+++ exercises/9.2/aula1-promises.js
@@ -1,11 +1,25 @@
 // Criando uma Promise e usando os parâmetros resolve e reject.
 
-const promise = new Promise((resolve, reject) => {
+/* const promise = new Promise((resolve, reject) => {
   const number = Math.floor(Math.random() * 11);
 
   if (number <= 5) {
     return reject(console.log(`Que fracasso =( Nosso número foi ${number})`));
   }
   resolve(console.log(`Que sucesso =) Nosso número foi ${number}`));
+}); */
+
+
+// Usando a função .then()
+
+const promise = new Promise((resolve, reject) => {
+  const number = Math.floor(Math.random() * 11);
+
+  if (number <= 5) {
+    return reject(console.log(`Que fracasso =( Nosso número foi ${number})`));
+  }
+  resolve(number);
 });
 
+promise.then(number => `Que sucesso =) Nosso número foi ${number}`)
+  .then(msg => console.log(msg));
