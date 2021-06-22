067803804b7ca12c7d6b474504364015f269c15b
Ed Santos <edsantos2822@gmail.com>
2021-06-22 12:25:15 -0300

    Creating Component.js

---
+++ exercises/front-end-React/11.1/second-exercise/src/Component.js
@@ -0,0 +1,10 @@
+function Component() {
+  return (
+    <div>
+      <h1>Edimar dos Santos</h1>
+      <p>Inteligente, paciente, sempre na busca pelo conhecimento</p>
+    </div>
+  );
+}
+
+export default Component;
