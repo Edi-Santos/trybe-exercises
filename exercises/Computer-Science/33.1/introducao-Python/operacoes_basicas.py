d65deee665df27159fdd7fc79a5699b20591a872
Ed Santos <edsantos2822@gmail.com>
2022-03-19 12:31:51 -0300

    Starting studies in Python - Basic Math operators

---
+++ exercises/Computer-Science/33.1/introducao-Python/operacoes_basicas.py
@@ -0,0 +1,14 @@
+# Exercício 1: No terminal, inicializa duas variáveis a e b,
+# sendo a = 10 e b = 5. Mostre o resultado das 7 operações básicas
+# (soma, subtração, multiplicação, divisão, divisão inteira,
+# potenciação e módulo) envolvendo essas variáveis.
+a = 10
+b = 5
+
+print("Soma:", a + b)
+print("Subtração:", a - b)
+print("Multiplicação:", a * b)
+print("Divisão:", a / b)
+print("Divisão inteira:", a // b)
+print("Potenciação:", a ** b)
+print("Módulo", a % b)
