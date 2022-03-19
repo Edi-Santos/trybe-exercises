494feb7bc8458588a3b464db28023137f93a1f16
Ed Santos <edsantos2822@gmail.com>
2022-03-19 12:44:02 -0300

    Completing exercise 2

--- exercises/Computer-Science/33.1/introducao-Python/operacoes_basicas.py
+++ exercises/Computer-Science/33.1/introducao-Python/operacoes_basicas.py
@@ -5,6 +5,7 @@
 a = 10
 b = 5
 
+print("\nExercício 1")
 print("Soma:", a + b)
 print("Subtração:", a - b)
 print("Multiplicação:", a * b)
@@ -12,3 +13,16 @@ print("Divisão:", a / b)
 print("Divisão inteira:", a // b)
 print("Potenciação:", a ** b)
 print("Módulo", a % b)
+
+# Exercício 2: Declare e inicialize uma variável: hours = 6 . Quantos minutos
+# têm em 6 horas? E quantos segundos? Declare e inicialize
+# variáveis minutes e seconds que recebem os respectivos resultados
+# das contas. Depois, imprima cada uma delas.
+hours = 6
+minutes = hours * 60
+seconds = minutes * 60
+
+print("\nExercício 2")
+print("Horas:", hours)
+print("Minutos:", minutes)
+print("Segundos:", seconds)
