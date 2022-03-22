4f83fef098377b81cda699fab497529d0bb4d2e6
Ed Santos <edsantos2822@gmail.com>
2022-03-21 22:25:03 -0300

    Completing exercise 2

--- exercises/Computer-Science/33.1/introducao-Python/exercicios.py
+++ exercises/Computer-Science/33.1/introducao-Python/exercicios.py
@@ -10,3 +10,14 @@ def maior_numero(a, b):
 
 printa_maior_numero = maior_numero(4, 2)
 print(f"\nExercício 1 - Printa o maior número: {printa_maior_numero}")
+
+# Exercício 2: Calcule a média aritmética dos valores contidos em uma lista.
+lista_numeros = [3, 4, 6, 8, 10, 5]
+tamanho_lista = len(lista_numeros)
+soma_numeros = 0
+
+for numero in lista_numeros:
+    soma_numeros += numero
+
+media = soma_numeros / tamanho_lista
+print(f"Exercício 2 - Calcula a média de uma lista: {media}")
