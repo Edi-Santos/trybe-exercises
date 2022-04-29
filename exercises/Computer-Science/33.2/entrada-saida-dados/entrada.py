492bae599bada1131d774aaea2b2f3f4e0a22f8c
Ed Santos <edsantos2822@gmail.com>
2022-04-29 19:30:56 -0300

    Doing a little exercise

--- exercises/Computer-Science/33.2/entrada-saida-dados/entrada.py
+++ exercises/Computer-Science/33.2/entrada-saida-dados/entrada.py
@@ -1,3 +1,5 @@
+import random
+
 # No Python nós temos a função input() como uma das formas de receber dados.
 # Esta função retorna sempre dados do tipo String.
 
@@ -13,3 +15,19 @@ convertido para 'int': "))
 
 print(f"Seu número convertido: {meu_numero_convertido}")
 print(f"Tipo do seu número: {type(meu_numero_convertido)}\n")
+
+
+# Criando exemplo prático
+
+# escolhe um número aleatório entre 1 e 10
+print("Testando na prática")
+guess = ""
+random_number = random.randint(1, 10)
+
+# enquanto o usuário não acertar este trecho é repetido
+while guess != random_number:
+    guess = int(input("Qual o seu palpite? "))
+    if guess != random_number:
+        print("Errou. Tente novamente.")
+
+print("Acertou! O númera sorteado era:", guess, "\n")
