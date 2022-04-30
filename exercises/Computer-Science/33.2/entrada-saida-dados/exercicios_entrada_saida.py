8cb928928a3ed03620c029a490fdfd0d423bdad9
Ed Santos <edsantos2822@gmail.com>
2022-04-30 01:09:27 -0300

    Completing exercise 1

---
+++ exercises/Computer-Science/33.2/entrada-saida-dados/exercicios_entrada_saida.py
@@ -0,0 +1,13 @@
+# Exercício 1 -  Faça um programa que solicite o nome de uma pessoa usuária e imprima-o na vertical. Exemplo:
+# F
+# U
+# L
+# A
+# N
+# O
+
+print("Exercício 1")
+nome = input("Digite seu nome: ")
+
+for letra in nome:
+    print(letra, "\n")
