16fba2263f8cf1127309fabd76bed657a4489101
Ed Santos <edsantos2822@gmail.com>
2022-05-02 23:55:37 -0300

    Adding explanation about construction of an Object/Entity

--- exercises/Computer-Science/34.1/introducao_ao_poo/primeira_entidade.py
+++ exercises/Computer-Science/34.1/introducao_ao_poo/primeira_entidade.py
@@ -1,6 +1,6 @@
 # Uma entidade é o mesmo que um objeto. Um objeto é uma representação de algo
 # do mundo físico para a programação. É algo que tem informações e
-# comportamentos
+# comportamentos.
 
 # Criando o primeiro objeto
 class User:
@@ -8,3 +8,14 @@ class User:
         self.name = name
         self.email = email
         self.password = password
+# Está pronto o nosso objeto/entidade
+# Nós usamos a palavra reservada "class" para criarmos nossos objetos que
+# devem ser sempre abstratos, algo para ser reaproveitado depois.
+
+meu_user = User("Eren Jaeger", "erendotita@gmail.com", "FrenchFries")  # Aqui
+# nós estamos reaproveitando o nosso objeto User para criar um novo usuário e
+# atribuindo à variável "meu_user". Sendo assim, nossa variável é um objeto.
+print(meu_user)
+print(meu_user.name)
+print(meu_user.email)
+print(meu_user.password)
