47b13520878e513c0281ef5f08cfa43d47b5384f
Ed Santos <edsantos2822@gmail.com>
2022-05-03 19:25:27 -0300

    Adding more explanation abou POO

--- exercises/Computer-Science/34.1/introducao_ao_poo/primeira_entidade.py
+++ exercises/Computer-Science/34.1/introducao_ao_poo/primeira_entidade.py
@@ -39,3 +39,34 @@ print(meu_user.password)
 #         self.name = name
 #         self.email = email
 #         self.password = password
+
+
+# Para adicionar um comportamento nós criamos outra função dentro da classe
+# Exemplo de um usuário com o comportamento para mudar a senha:
+class User2:
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+    def reset_password(self):
+        print("Envia email de reset de senha")
+
+meu_user2 = User2("Mikasa Ackerman", "mikasa@ackerman.com", "French123")
+meu_user2.reset_password()
+# Obs.: A repetição de código aqui é apenas para fins didáticos. Esta não é
+# uma boa prática.
+
+# Nós podemos chamar uma função da classe também a partir do objeto que criamos
+# assim como acessar suas propriedades.
+
+# Aqui nós vemos uma grande vantagem na Programação Orientada a Objetos que é
+# a de reaproveitar código e de não precisarmos saber afundo como ela funciona.
+# Se a classe tem um nome e um comportamento/método (função) bem sugestivos, é
+# suficiente para que possamos usá-la, economizando tempo de ter que entender
+# como tal classe funciona.
+
+# Agora nós estendemos de uma vez dois do quatro pilares de POO que são:
+# Abstração e Encapsulamento. Abstração é quando a gente define Entidade/Objetos,
+# e o Encapsulamento é usarmos essas Entidades/Objetos sem, necessáriamente,
+# entender como elas funcionam.
